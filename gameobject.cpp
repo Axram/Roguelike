933fc265ceffed1f05006c78c44b06c9e0888e83
Gabriel Andersson Santiago <gabras@kth.se>
2015-12-11 13:27:18 +0100

    Martins Commit

--- gameobject.cpp
+++ gameobject.cpp
@@ -1,63 +1,22 @@
 #include <string>
+#include "gameobject.hpp"
 
-struct Pos{
-  int x;
-  int y;
-};
+Gameobject::Gameobject(){}
+Gameobject::Gameobject(int px, int py){
+  _pos.x = px;
+  _pos.y = py;
+}
+Gameobject::Gameobject(int px, int py, char img, bool solid, bool movable, std::string name, std::string desc){
+  _pos.x = px;
+  _pos.y = py;
+  _img = img;
+  _solid = solid;
+  _movable = movable;
+  _name = name;
+  _description = desc;
+}
 
-class Gameobject{
-public:
 
-  Gameobject(){}
-  Gameobject(int px, int py){
-    _pos.x = px;
-    _pos.y = py;
-  }
-  Gameobject(int px, int py, std::string img){
-    _pos.x = px;
-    _pos.y = py;  
-    _img = img;
-  }
-  Gameobject(int px, int py, std::string img, std::string name, std::string desc){
-    _pos.x = px; 
-    _pos.y = py;
-    _img = img;
-    _name = name;
-    _description = desc;
-  }
-  Gameobject(int px, int py, std::string img, bool solid, bool movable){
-    _pos.x = px;
-    _pos.y = py;
-    _img = img;
-    _solid = solid;
-    _movable = movable;
-  }
-  Gameobject(int px, int py, std::string img, bool solid, bool movable, std::string name, std::string desc){
-    _pos.x = px;
-    _pos.y = py;
-    _img = img;
-    _solid = solid;
-    _movable = movable;
-    _name = name;
-    _description = desc;
-  }
-  struct Pos _pos;
-  bool _solid = true;
-  bool _movable = false;
-  std::string _name = "Nobody";
-  std::string _description = "Caped baldy";
-  std::string _img = "@";
-private:
-
-  /*
-    Position (posx, posy)
-    Visuell Representation ("@")
-    Solid/Kan gås igenom (solid = true)
-    Namn (name = "Troll")
-    Beskrivning ("A smelly troll") 
-   */
-
-};
 
 
 
