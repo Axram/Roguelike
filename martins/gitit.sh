d4bc00903331c88ecd3549de66ecc8f9ed01cfda
Martin <martin.favre22@gmail.com>
2015-12-11 10:52:48 +0100

    Martins commit

---
+++ martins/gitit.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+git add README.md
+git add gameobject.cpp
+git add actor.cpp
+git add player.cpp
+git add npc.cpp
+git add goblin.cpp
+git add item.cpp
+git add potion.cpp
+git add equipment.cpp
+git add sword.cpp
+git add wall.cpp
+git add map.cpp
+rm -r martins
+mkdir martins
+cp makefile martins/makefile
+cp gitpull.sh martins/gitpull.sh
+cp gitit.sh martins/gitit.sh
+git add martins
+rm -r martins
+git commit -m  "Martins commit"
+git push origin master
